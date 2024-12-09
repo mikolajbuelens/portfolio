bd0b87813a30f22dda299cf9ba0285a707959c8f
MikolajBuelens <mikolaj.buelens@student.ehb.be>
2024-12-09 03:13:42 +0100

    feat: add prototypes and description to projects

--- portfolio/src/components/ProjectCard.jsx
+++ portfolio/src/components/ProjectCard.jsx
@@ -3,12 +3,20 @@ export default function ProjectCard({ project }) {
     <div className="project-card">
       <img className="projectImg" src={project.image} alt="" />
       <h3>{project.title}</h3>
-
+      <p className="project-description">{project.description}</p>
       {/* <p>{project.description}</p> */}
       <div className="links">
-        <a target="blank" className="discover" href={project.url}>
-          Discover
+        {/* if project url = "" don't show */}
+
+        <a
+          target="blank"
+          className={project.url === "" ? "hidden" : "discover"}
+          href={project.url}
+        >
+          Visit
         </a>
+        {/* Visit
+        </a> */}
         <a target="blank" href={project.github}>
           <img src="github-mark-white.svg" alt="" />
         </a>
